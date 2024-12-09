c8b140079f455690bc9542e2131190fa17b52355
Brandon <93629403+hairforce1x@users.noreply.github.com>
2024-12-09 11:27:25 -0700

    Finished part 4.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26";
 
-strToArray = csv.split("\n"); // converting the long string into an array w strings inside
-
-let veryFinePeople = [];
-for (i=0; i<strToArray.length; i++){
+let strToArray = csv.split("\n");
+let cachedArray = [];
+for (let i = 0; i < strToArray.length; i++) {
     let row = strToArray[i].split(",");
-    veryFinePeople.push(row);
+    cachedArray.push(row);
 }
-console.log(veryFinePeople);
+
+
+// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
+// Convert these keys to all lowercase letters for consistency.
+// Store these objects in an array, in the order that they were originally listed.
+// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
+
+let headers = cachedArray[0].toString().toLowerCase().split(",");
+let dataObjects = cachedArray.slice(1).map(function (row) {
+    let obj={};
+   row.forEach(function(value, index) {
+        obj[headers[index]] = value;
+    });
+    return obj;
+})
+
